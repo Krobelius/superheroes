d7739b435da4b23ddbfa632f5172b29cdbc86332
Krobelius <oshargorodskiy@list.ru>
2021-11-13 22:41:44 +0300

    super_3

--- lib/widgets/superhero_card.dart
+++ lib/widgets/superhero_card.dart
@@ -2,14 +2,14 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:superheroes/blocs/main_bloc.dart';
 import 'package:superheroes/resources/superheroes_colors.dart';
+import 'package:superheroes/resources/superheroes_images.dart';
 
 class SuperheroCard extends StatelessWidget {
   final SuperheroInfo superheroInfo;
   final VoidCallback onTap;
 
   const SuperheroCard(
-      {Key? key,
-      required this.superheroInfo, required this.onTap})
+      {Key? key, required this.superheroInfo, required this.onTap})
       : super(key: key);
 
   @override
@@ -19,28 +19,55 @@ class SuperheroCard extends StatelessWidget {
       child: Container(
         height: 70,
         clipBehavior: Clip.antiAlias,
-        decoration:  BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: SuperheroesColors.card
-        ),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: SuperheroesColors.card),
         child: Row(
           children: [
-            CachedNetworkImage(imageUrl: superheroInfo.imageUrl,height: 70,width: 70,fit: BoxFit.cover,),
+            Container(
+                width: 70,
+                height: 70,
+                color: Colors.white24,
+                child: CachedNetworkImage(
+                  errorWidget: (context, url, error) {
+                    return Center(
+                      child: Image.asset(
+                        SuperheroesImages.unknown,
+                        width: 20,
+                        height: 62,
+                      ),
+                    );
+                  },
+                  progressIndicatorBuilder: (context, url, downloadProgress) =>
+                      SizedBox(
+                          width: 24,
+                          height: 24,
+                          child: Center(
+                              child: CircularProgressIndicator(
+                                  value: downloadProgress.progress))),
+                  imageUrl: superheroInfo.imageUrl,
+                  height: 70,
+                  width: 70,
+                  fit: BoxFit.cover,
+                )),
             const SizedBox(width: 12),
             Expanded(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(superheroInfo.name.toUpperCase(),style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700
-                  ),),
-                  Text(superheroInfo.realName,style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400))
+                  Text(
+                    superheroInfo.name.toUpperCase(),
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700),
+                  ),
+                  Text(superheroInfo.realName,
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400))
                 ],
               ),
             )
